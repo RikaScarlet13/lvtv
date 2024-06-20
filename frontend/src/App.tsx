b83091281dba3b8903f5737eab07388b3d514c2d
khaihoulaw <jameslaw@student.laverdad.edu.ph>
2024-06-20 12:08:43 +0800

    added alot of features in the front end

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useLocation,
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    useLocation,
 } from "react-router-dom";
 import { AuthProvider, useAuth } from "./context/AuthContext";
 import LandingPage from "./pages/LandingPage";
@@ -13,6 +13,13 @@ import LoginPage from "./pages/LoginPage";
 import Navbar from "./components/Navbar";
 import AdminLoginPage from "./pages/admin/LoginPage";
 import Header from "./components/admin/Header";
+import Archives from "./pages/Archives";
+import OurStory from "./pages/OurStory";
+import Teleradio from "./pages/Teleradio";
+import Bab from "./pages/courses/Bab";
+import BsaBsais from "./pages/courses/BsaBsais";
+import BsisAct from "./pages/courses/BsisAct";
+import Bssw from "./pages/courses/Bssw";
 
 // const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
 //   const { isAuthenticated } = useAuth();
@@ -20,43 +27,52 @@ import Header from "./components/admin/Header";
 // };
 
 const App = () => {
-  // const location = useLocation(); // Step 1: Get the current location
+    // const location = useLocation(); // Step 1: Get the current location
 
-  // const isAdminRoute = location.pathname.startsWith("/admin"); // Step 2: Check if it's an admin route
-  return (
-    <Router>
-      {/* <Navbar /> */}
-      {/* {!isAdminRoute && <Navbar />} */}
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        {/* <Route path="/login" element={<LoginPage />} /> */}
-        <Route path="/admin/login" element={<AdminLoginPage />} />
+    // const isAdminRoute = location.pathname.startsWith("/admin"); // Step 2: Check if it's an admin route
+    return (
+        <Router>
+            {/* <Navbar /> */}
+            {/* {!isAdminRoute && <Navbar />} */}
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                {/* <Route path="/login" element={<LoginPage />} /> */}
+                <Route path="/admin/login" element={<AdminLoginPage />} />
 
-        {/* MOVE TO PROTECTED ROUTES */}
+                <Route path="/Archives" element={<Archives />} />
+                <Route path="/HomePage" element={<HomePage />} />
+                <Route path="/OurStory" element={<OurStory />} />
+                <Route path="/Teleradio" element={<Teleradio />} />
+                <Route path="courses/Bab" element={<Bab />} />
+                <Route path="courses/BsaBsais" element={<BsaBsais />} />
+                <Route path="courses/BsisAct" element={<BsisAct />} />
+                <Route path="courses/Bssw" element={<Bssw />} />
 
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/admin/dashboard" element={<Dashboard />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/admin/dashboard" element={<Dashboard />} />
-        {/* <Route path="/admin/dashboard" element={<Header />} /> */}
+                {/* MOVE TO PROTECTED ROUTES */}
 
-        {/* END MOVE TO PROTECTED ROUTES */}
-        <Route
-          path="/app"
-          element={
-            <AuthProvider>
-              <ProtectedRoutes />
-            </AuthProvider>
-          }
-        />
-        {/* <Route path="/admin/login" element={<AdminLoginPage />} /> */}
-      </Routes>
-    </Router>
-  );
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/admin/dashboard" element={<Dashboard />} />
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/admin/dashboard" element={<Dashboard />} />
+                {/* <Route path="/admin/dashboard" element={<Header />} /> */}
+
+                {/* END MOVE TO PROTECTED ROUTES */}
+                <Route
+                    path="/app"
+                    element={
+                        <AuthProvider>
+                            <ProtectedRoutes />
+                        </AuthProvider>
+                    }
+                />
+                {/* <Route path="/admin/login" element={<AdminLoginPage />} /> */}
+            </Routes>
+        </Router>
+    );
 };
 
 const ProtectedRoutes = () => {
-  return <Routes>{/* Add more authenticated routes here */}</Routes>;
+    return <Routes>{/* Add more authenticated routes here */}</Routes>;
 };
 
 export default App;
